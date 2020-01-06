fb427ad4f59eb3316b483d9179a2226a0166e7e4
totobitbucket <diavolos1230@gmail.com>
2020-01-07 00:05:57 +0900

    【Unity講座第一回用ゲーム】 ・オリジナルゲーム ・PC用

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement movement;
+
+    private GameObject getText1;
+    private GameObject getText2;
+
+    private void Start()
+    {
+        this.getText1 = GameObject.Find("GameClearText");
+        this.getText2 = GameObject.Find("GameOverText");
+    }
+
+    private void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            this.getText2.GetComponent<Text>().text = "Game Over";
+            movement.enabled = false;
+        }
+
+        if (collisionInfo.collider.tag == "ClearTag")
+        {
+            this.getText1.GetComponent<Text>().text = "Game Clear!!";
+            movement.enabled = false;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerFall.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayerFall : MonoBehaviour
+{
+    public PlayerMovement movement;
+
+    private GameObject getText2;
+
+    //private bool isGameOver = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.getText2 = GameObject.Find("GameOverText");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float bottomY = gameObject.transform.position.y;
+        float bottomX = gameObject.transform.rotation.x;
+
+        if(gameObject.transform.position.y < 0.0)
+        {
+            this.getText2.GetComponent<Text>().text = "Game Over";
+            movement.enabled = false;
+        }
+
+
+
+        //if (this.isGameOver == true)
+        //{
+        if (Input.GetKey(KeyCode.Space))
+            {
+                SceneManager.LoadScene("MainScene");
+            }
+        //}
+    }
+
+
+}
