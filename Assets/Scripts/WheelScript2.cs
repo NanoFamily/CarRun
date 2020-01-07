612f3e3429c1a7fb345717dece4ae505ea6f5b12
totobitbucket <diavolos1230@gmail.com>
2020-01-07 12:58:42 +0900

    【Unity講座第1回目用】 ・オリジナルゲーム ・PC用

--- Assets/Scripts/WheelScript2.cs
+++ Assets/Scripts/WheelScript2.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WheelScript : MonoBehaviour
+public class WheelScript2 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
